142ffdf7ac1ba16d3ff2c8e8c101ccda7f151e6b
satpreets234 <87688879+satpreets234@users.noreply.github.com>
2023-11-27 13:16:54 +0530

    Update connection.js

--- backend/connection/connection.js
+++ backend/connection/connection.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 async function connection (){
-    mongoose.connect('mongodb+srv://root:root@cluster0.rhtxjhj.mongodb.net/url_db').then((success)=>{
+    mongoose.connect(process.env.MONGO_URI).then((success)=>{
         console.log('Database connection established!');
         }).catch((error)=>{
             console.log(error);
